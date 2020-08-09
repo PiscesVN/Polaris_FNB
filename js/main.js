6cefdec1204fd9fac2ca4e524f4daff0c7e0cd6a
thuongntn <thuongntn1998@gmail.com>
2020-08-08 23:29:04 -0700

    3. add page features

--- js/main.js
+++ js/main.js
@@ -14,4 +14,17 @@ $(document).ready(function () {
 
     $('.animated-icon3').toggleClass('open');
   });
+
+  // $('#demo').click(function(){
+  // 	if($('i.icon').hasClass("fa-chevron-down")){
+	 //  	$('i.icon').removeClass("fa-chevron-down");
+	 //  	$('i.icon').addClass("fa-chevron-up");
+  // 	}
+  // 	else{
+  // 		$('i.icon').addClass("fa-chevron-down");
+	 //  	$('i.icon').removeClass("fa-chevron-up");
+  // 	}
+  // });
 });
+
+// js for click menu toggle
